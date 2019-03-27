ae7cc0bba3eb2515be2494da08ce2653153b349a
sshhane <g00332592@gmit.ie>
2019-03-27 17:37:45 +0000

    added menu scene

---
+++ swiftApp/spaceGame/spaceGame/MenuScene.swift
@@ -0,0 +1,14 @@
+//
+//  MenuScene.swift
+//  spaceGame
+//
+//  Created by Shane Daniels on 27/03/2019.
+//  Copyright © 2019 Shane Daniels. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class MenuScene: SKScene {
+
+}
