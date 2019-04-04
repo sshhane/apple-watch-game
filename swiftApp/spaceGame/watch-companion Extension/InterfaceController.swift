f816d4e8e78c9b7951bf26f98a67045ffc7050a2
sshhane <g00332592@gmit.ie>
2019-04-04 17:38:44 +0100

    displaying watch roll attitude data on watch face

--- swiftApp/spaceGame/watch-companion Extension/InterfaceController.swift
+++ swiftApp/spaceGame/watch-companion Extension/InterfaceController.swift
@@ -9,19 +9,39 @@
 import WatchKit
 import Foundation
 import WatchConnectivity
+import CoreMotion
 
 class InterfaceController: WKInterfaceController {
+    
+    // label
+    @IBOutlet weak var roll: WKInterfaceLabel!
+    
+    // vars
+    var rollStr = ""
+    
+    // motion manager
+    let motionManager = CMMotionManager()
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
         // Configure interface objects here.
         reloadData(move: "left")
+        
+        motionManager.startDeviceMotionUpdates(to: OperationQueue.current!) { (data, error) in
+//            print(data as Any)
+            if let d = data {
+                let r = d.attitude.roll
+                self.rollStr = "Roll:  \(Double(r))"
+            }
+            self.updateLabel()
+        }
     }
     
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        updateLabel()
     }
     
     override func didDeactivate() {
@@ -43,10 +63,14 @@ class InterfaceController: WKInterfaceController {
     //
     @IBAction func moveLeft() {
         reloadData(move: "left")
+        self.rollStr = "left"
+        updateLabel()
     }
     
     @IBAction func moveRight() {
         reloadData(move: "right")
+        self.rollStr = "right"
+        updateLabel()
     }
     
     private func sendMove(move: String) {
@@ -55,8 +79,18 @@ class InterfaceController: WKInterfaceController {
             session.delegate = self as! WCSessionDelegate
             session.activate()
         }
-        
+    }
+    
+    func updateLabel() {
+        roll.setText(rollStr)
+    }
+    
+    func startDeviceMotionUpdates() {
         
     }
     
+//    var attitude: CMAttitude { get }
+    
+//    let motionManager = CMMotionManager()
+//    motionManager.deviceMotionUpdateInterval = 1.0 / 50
 }
